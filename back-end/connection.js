6098489700c52e1707e68c0f2f44fbf4de7c25b3
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-06-21 01:40:40 -0300

    refactor

---
+++ back-end/connection.js
@@ -0,0 +1,21 @@
+const {Client} = require('pg')
+/*
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "admin",
+    database: "postgres",
+    schema: 'public'
+})
+*/
+const client = new Client({
+    host: "devweb.crnmq06dvith.us-east-1.rds.amazonaws.com",
+    user: "postgres",
+    port: 5432,
+    password: "12345678",
+    database: "postgres",
+    schema: "previdencia_social"
+})
+
+module.exports = client
