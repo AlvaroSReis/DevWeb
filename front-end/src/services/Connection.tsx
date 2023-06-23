d6aa07c1b57e01e34e1ec85fcaaebdf3fc177430
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-06-23 18:47:06 -0300

    fix

--- front-end/src/services/Connection.tsx
+++ front-end/src/services/Connection.tsx
@@ -1,19 +1,25 @@
 import Api from "./Api";
 
 export async function query1(){
-    //let data: string;
-    await Api.get("/query1").then((result) =>
-    console.log(result))
-    //data = JSON.stringify(Api.get("/query1"));
-    //console.log(data)
+    let data: string;
+    //await Api.get("/query1").then((result) =>
+    //console.log(result.data.rows))
+    data = JSON.stringify(await Api.get("/query1").then((result) => {
+        console.log(result)
+        return result.data.rows
+    }));
+    console.log(data)
     //return data;
 }
 
 export async function query3(){
-    //let data: string;
-    await Api.get("/query3").then((result) =>
-    console.log(result))
-    //data = JSON.stringify(Api.get("/query1"));
-    //console.log(data)
+    let data: string;
+    //await Api.get("/query1").then((result) =>
+    //console.log(result.data.rows))
+    data = JSON.stringify(await Api.get("/query3").then((result) => {
+        console.log(result)
+        return result.data.rows
+    }));
+    console.log(data)
     //return data;
 }
