d38c1f027d359552c00fa80c197979d058369269
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-06-23 14:19:55 -0300

    refactor

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import {query1} from './services/Connection';
+import {query1, query3} from './services/Connection';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Para checar o resultado veja o console do navegador.</h1>
         <button onClick={query1}> query1</button>
+        <button onClick={query3}> query3</button>
       </header>
     </div>
   );
--- front-end/src/services/Connection.tsx
+++ front-end/src/services/Connection.tsx
@@ -8,3 +8,12 @@ export async function query1(){
     //console.log(data)
     //return data;
 }
+
+export async function query3(){
+    //let data: string;
+    await Api.get("/query3").then((result) =>
+    console.log(result))
+    //data = JSON.stringify(Api.get("/query1"));
+    //console.log(data)
+    //return data;
+}
