109a3cc75b75ecde6b2acaedfa0bf1612b511454
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-06-26 02:52:23 -0300

    fix

--- front-end/src/services/Api.tsx
+++ front-end/src/services/Api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 1000,
-    headers: {'Keep-Alive': 'true'}
+    //headers: {'Keep-Alive': 'true'}
   });
 
   export default api;
