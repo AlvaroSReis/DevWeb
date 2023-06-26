1937ee9264752a2c4702150c9c5801ac403efbf7
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-06-26 02:58:17 -0300

    refactor

--- front-end/src/services/Api.tsx
+++ front-end/src/services/Api.tsx
@@ -1,9 +1,16 @@
 import axios from "axios";
-
+/*
 const api = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 1000,
     //headers: {'Keep-Alive': 'true'}
   });
+  */
+  const api = axios.create({
+    baseURL: 'https://devweb-5sm9.onrender.com/',
+    timeout: 1000,
+    //headers: {'Keep-Alive': 'true'}
+  });
+  
 
   export default api;
