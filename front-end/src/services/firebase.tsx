e2a5426a112e7f6aa2b1fa25cab9ad47b0de0550
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2023-07-25 21:08:48 -0300

    feature
    firebase auth initial config.

---
+++ front-end/src/services/firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+//const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASECONFIG)
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDji2Gg9J0v7uJWG4uCLOsHu4fHztiY1r0",
+    authDomain: "dctpv-devweb.firebaseapp.com",
+    projectId: "dctpv-devweb",
+    storageBucket: "dctpv-devweb.appspot.com",
+    messagingSenderId: "77245788805",
+    appId: "1:77245788805:web:5c70bfa22b440a50006fec"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
